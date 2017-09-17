7447b2ce3fad11d680d359b4faedd1445bf17b16
LauJ0 <31933183+LauJ0@users.noreply.github.com>
2017-09-17 11:04:15 -0400

    Update HelloSync.java

--- src/HelloSync.java
+++ src/HelloSync.java
@@ -3,7 +3,9 @@ public class HelloSync {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// Say hello
 		System.out.print("Hello from school");
 	}
-
+		System.out.print("Hello from home");
 }
